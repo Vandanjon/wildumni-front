759f29b2a77fee2c5648361c5c90ae3bcf4eb553
Splagadou <benoit@vandanjon.fr>
2023-01-05 11:57:32 +0100

    make context usable in the whole App

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import UserExport from "./assets/contexts/UserContext";
 
 import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <UserExport.ContextProvider>
+            <App />
+        </UserExport.ContextProvider>
     </React.StrictMode>
 );
