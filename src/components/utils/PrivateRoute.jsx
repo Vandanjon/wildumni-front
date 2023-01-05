3500251db740f7f20c8ee9ab7a9f58a36bd785c9
Splagadou <benoit@vandanjon.fr>
2023-01-05 10:20:41 +0100

    role ADMIN is checked on AdminView

--- src/components/utils/PrivateRoute.jsx
+++ src/components/utils/PrivateRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
-const ProtectedRoute = ({ user, children }) => {
-    if (!user) {
+const PrivateRoute = ({ children }) => {
+    const token = sessionStorage.getItem("token");
+    const decoded = jwt_decode(token);
+
+    if (!decoded.roles.includes("ROLE_ADMIN")) {
         return <Navigate to="/" replace />;
     }
 
