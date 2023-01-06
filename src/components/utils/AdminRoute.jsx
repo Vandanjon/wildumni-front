5a2de4c2f01fd5750b37b26e8b8cf0f92e0426e7
Splagadou <benoit@vandanjon.fr>
2023-01-06 09:42:19 +0100

    fixing loop on calls for routes layouts + reroute users with correct roles + add test button on ViewUser.jsx

---
+++ src/components/utils/AdminRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+
+const AdminRoute = ({ children }) => {
+    const { user } = useContext(UserContext);
+
+    if (!user) {
+        return <Navigate to="/" replace />;
+    } else if (user.includes("ROLE_ADMIN")) {
+        return children;
+    } else {
+        return <Navigate to="/user" replace />;
+    }
+};
+
+export default AdminRoute;
--- src/pages/ViewUser.jsx
+++ src/pages/ViewUser.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
+
 const ViewUser = () => {
+    const { user } = useContext(UserContext);
+    console.log(user);
     return (
         <>
             <h1>ViewUserPage</h1>
             <p>coucou</p>
+            <Link to="/admin">ADMIN</Link>
         </>
     );
 };
