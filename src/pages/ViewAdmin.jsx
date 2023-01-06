c34f5cd816fcf2ef9aa6a67a8fd592ccd1616051
Splagadou <benoit@vandanjon.fr>
2023-01-06 12:40:53 +0100

    add some datas to test Get route on ViewAdmin

--- src/pages/ViewAdmin.jsx
+++ src/pages/ViewAdmin.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const ViewAdmin = () => {
-	return (
-		<>
-			<h1>ViewAdminPage</h1>
-			<p>coucou</p>
-		</>
-	);
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8001/users/")
+            .then((res) => {
+                console.log(res.data);
+                setUsers(res.data);
+            })
+            .catch((err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+    }, []);
+
+    return (
+        <>
+            <h1>Admin Panel</h1>
+
+            {users
+                ? users.map((user) => (
+                      <section>
+                          <article>
+                              <h1>{user.firstName}</h1>
+                              <p>{user.email}</p>
+                              <p>{user.address.country}</p>
+                          </article>
+
+                          <button>EDIT</button>
+
+                          <button>DELETE</button>
+                      </section>
+                  ))
+                : ""}
+        </>
+    );
 };
 
 export default ViewAdmin;
