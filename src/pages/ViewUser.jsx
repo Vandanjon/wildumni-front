da08973e45b0ffe21e506e6848e06ad761ba886b
Splagadou <benoit@vandanjon.fr>
2023-01-08 22:23:06 +0100

    working button outside the MapContainer. Improvements required to sanitize the code

--- src/pages/ViewUser.jsx
+++ src/pages/ViewUser.jsx
@@ -1,6 +1,38 @@
-import { useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import NavBar from "../components/NavBar";
+import PersonalMap from "../components/PersonalMap";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const center = [51.505, -0.09];
+const zoom = 13;
+
+function DisplayPosition({ map }) {
+    const [position, setPosition] = useState(() => map.getCenter());
+
+    const onClick = useCallback(() => {
+        map.setView(center, zoom);
+    }, [map]);
+
+    const onMove = useCallback(() => {
+        setPosition(map.getCenter());
+    }, [map]);
+
+    useEffect(() => {
+        map.on("move", onMove);
+        return () => {
+            map.off("move", onMove);
+        };
+    }, [map, onMove]);
+
+    return (
+        <p>
+            {/* latitude: {position.lat.toFixed(4)}, longitude:{" "}
+            {position.lng.toFixed(4)}  */}
+            <button onClick={onClick}>locate me</button>
+        </p>
+    );
+}
 
 const ViewUser = () => {
     const { user } = useContext(UserContext);
@@ -11,13 +43,29 @@ const ViewUser = () => {
         },
     ]);
 
+    const [map, setMap] = useState(null);
+
+    const displayMap = useMemo(
+        () => (
+            <MapContainer center={center} zoom={zoom} ref={setMap}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        ),
+        []
+    );
+
     return (
         <div id="UserPage" className="pageContainer">
             <header>
                 <NavBar />
+
+                <div>{map ? <DisplayPosition map={map} /> : null}</div>
             </header>
 
-            <main>Welcome</main>
+            <main>{displayMap}</main>
             <footer>
                 <p>{profile[0].firstName}</p>
 
