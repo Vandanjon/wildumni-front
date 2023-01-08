bde8b90b465e0e4c35c821cae6a811173bd929eb
Splagadou <benoit@vandanjon.fr>
2023-01-08 11:26:53 +0100

    HotFix on routes calls in App.jsx

--- src/pages/ViewUser.jsx
+++ src/pages/ViewUser.jsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
-import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const ViewUser = () => {
     const { user } = useContext(UserContext);
-    console.log(user);
+
     return (
         <div id="UserPage" className="pageContainer">
             <section className="header">
                 <NavBar />
             </section>
+
             <section className="main">Weclome</section>
             <section className="footer">Welcome</section>
         </div>
